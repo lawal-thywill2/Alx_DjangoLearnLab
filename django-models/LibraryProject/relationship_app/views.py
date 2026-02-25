ab73ab819fe06a0769456a61ca2836a51f1bd4df
lawal thywill <lawalkanyinsola889@gmail.com>
2026-02-25 08:08:37 -0800

    ...

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -27,7 +27,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy("login")
     template_name = "relationship_app/register.html"
 
