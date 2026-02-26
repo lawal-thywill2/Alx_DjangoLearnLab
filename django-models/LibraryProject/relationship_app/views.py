1fd626b3a4059271c8479b2f5189f6e6fe767dd0
lawal thywill <lawalkanyinsola889@gmail.com>
2026-02-26 14:29:41 -0800

    ..

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -39,3 +39,35 @@ def LoginView(request):
 
 def LogoutView(request):
     return render(request, 'relationship_app/logout.html')
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+
+# role check functions
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'admin'
+
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'librarian'
+
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'member'
+
+
+# views
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
