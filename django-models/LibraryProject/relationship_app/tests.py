77ced4b40d366554801a73a87a0b128181e0bf1b
lawal thywill <lawalkanyinsola889@gmail.com>
2026-02-26 14:26:36 -0800

    ''

--- django-models/LibraryProject/relationship_app/tests.py
+++ django-models/LibraryProject/relationship_app/tests.py
@@ -1,6 +1,25 @@
 from django.test import TestCase
-from LibraryProject.relationship_app.admin_view import is_admin
-
-
 # Create your tests here.
-@user_passes_test(is_admin)
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class RoleBasedViewTests(TestCase):
+
+    def setUp(self):
+        # Create a user
+        self.user = User.objects.create_user(username='testuser', password='pass123')
+
+        # Set role to admin (default test role)
+        self.user.userprofile.role = 'admin'
+        self.user.userprofile.save()
+
+    def test_admin_view_access(self):
+        # Log in the user
+        self.client.login(username='testuser', password='pass123')
+
+        # Try to access admin view
+        response = self.client.get(reverse('admin-view'))
+
+        # Check that access is allowed
+        self.assertEqual(response.status_code, 200)
