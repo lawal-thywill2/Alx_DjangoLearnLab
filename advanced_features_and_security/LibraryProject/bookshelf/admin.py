d1ea3abdfd95a7ed4b258e4690c3edcb375e0c6b
lawal thywill <lawalkanyinsola889@gmail.com>
2026-02-27 01:52:47 -0800

    ..

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -13,3 +13,17 @@ class BookAdmin(admin.ModelAdmin):
 
 # Register model with custom admin
 admin.site.register(Book, BookAdmin)
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Information", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
