07da4f59b900500ca406e2b90c32c3a836a85f59
lawal thywill <lawalkanyinsola889@gmail.com>
2026-02-26 23:55:14 -0800

    last model file

--- django-models/LibraryProject/relationship_app/templates/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/templates/relationship_app/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 from . import admin_view, librarian_view, member_view
 
 urlpatterns = [
     path('', views.home, name='home'),  # your home page
-    path('login/', views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register_view, name='register'),
     path('library/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('books/', views.list_books, name='list_books'),
     path('admin-dashboard/', admin_view.admin_view, name='admin-view'),
     path('librarian-dashboard/', librarian_view.librarian_view, name='librarian-view'),
     path('member-dashboard/', member_view.member_view, name='member-view'),
+    path('add/', views.add_book, name='add-book'),
+    path('edit/', views.edit_book, name='edit-book'),
+    path('delete/', views.delete_book, name='delete-book'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -71,3 +71,20 @@ def librarian_view(request):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
+from django.contrib.auth.decorators import permission_required
+
+
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    return render(request, 'relationship_app/add_book.html')
+
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request):
+    return render(request, 'relationship_app/edit_book.html')
+
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request):
+    return render(request, 'relationship_app/delete_book.html')
