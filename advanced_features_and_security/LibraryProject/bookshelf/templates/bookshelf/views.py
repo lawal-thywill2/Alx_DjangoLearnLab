439b4e71b65a8977ad5f7b9f960d4970a9130d4a
lawal thywill <lawalkanyinsola889@gmail.com>
2026-02-27 03:15:56 -0800

    ..

--- advanced_features_and_security/LibraryProject/bookshelf/templates/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/templates/bookshelf/views.py
@@ -6,22 +6,22 @@ from django.contrib.auth.decorators import permission_required
 from .models import Book
 
 
-@permission_required('relationship_app.can_view', raise_exception=True)
+@permission_required('bookstore.can_list', raise_exception=True)
 def view_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/view_books.html', {'books': books})
+    return render(request, 'bookstore/book_list.html', {'books': books})
 
 
-@permission_required('relationship_app.can_create', raise_exception=True)
+@permission_required('bookstore.can_create', raise_exception=True)
 def create_book(request):
-    return render(request, 'relationship_app/create_book.html')
+    return render(request, 'bookstore/create_book.html')
 
 
-@permission_required('relationship_app.can_edit', raise_exception=True)
+@permission_required('bookstore.can_edit', raise_exception=True)
 def edit_book(request):
-    return render(request, 'relationship_app/edit_book.html')
+    return render(request, 'bookstore/edit_book.html')
 
 
-@permission_required('relationship_app.can_delete', raise_exception=True)
+@permission_required('bookstore.can_delete', raise_exception=True)
 def delete_book(request):
-    return render(request, 'relationship_app/delete_book.html')
+    return render(request, 'bookstore/delete_book.html')
