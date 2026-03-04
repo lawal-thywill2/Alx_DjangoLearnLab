09ca9f676d0ec7f3aaba4e9f2fd6b7787459e2cb
lawal thywill <lawalkanyinsola889@gmail.com>
2026-03-04 14:12:54 -0800

    ..

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -67,7 +67,7 @@ class LikePostView(generics.GenericAPIView):
 
     def post(self, request, pk):
         try:
-            post = Post.objects.get(pk=pk)
+            post = generics.get_or_404(Post, pk=pk)
         except Post.DoesNotExist:
             return Response({"error": "Post not found"}, status=status.HTTP_404_NOT_FOUND)
 
